57cc7ffdc912b073b76d89fec35664d1caf92fbc
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-08-06 11:54:07 +0000

    responsive header complete

--- src/App.js
+++ src/App.js
@@ -1,15 +1,59 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "./App.css";
-// import HomeScreen from "./screens/HomeScreen";
+import HomeScreen from "./screens/HomeScreen";
 
 function App() {
+  const toggleMenu = () => {
+    document.querySelector(".sidebar").classList.toggle("open");
+  };
   return (
-    <div className="App">
-      <div className="header">
-        <div className="brand"></div>
-        <div className="header-linkss"></div>
+    <Router>
+      <div className="App">
+        <header className="header">
+          <div className="brand">
+            <Link to="/">RichHomes</Link>
+          </div>
+          <div className="header-links">
+            <Link>Home</Link>
+            <Link>Properties</Link>
+            <Link>About Us</Link>
+            <Link>FAQ</Link>
+            <Link>Contact</Link>
+            <button
+              id="head-btn"
+              className="header-button"
+              onClick={toggleMenu}
+            >
+              &#9776;
+            </button>
+          </div>
+        </header>
+        <aside className="sidebar">
+          <ul>
+            <li>
+              <Link>Home</Link>
+            </li>
+            <li>
+              <Link>Properties</Link>
+            </li>
+            <li>
+              <Link>About Us</Link>
+            </li>
+            <li>
+              <Link>FAQ</Link>
+            </li>
+            <li>
+              <Link>Contact</Link>
+            </li>
+          </ul>
+        </aside>
+
+        <div>
+          <Route path="/" component={HomeScreen} exact={true} />
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
