f3cc95985b72bb3b58c2dd5a383a2393325ecb9f
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-08-06 12:58:13 +0000

    hero section

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,7 +1,18 @@
 import React from "react";
+import "./HomeScreen.css";
 
 function HomeScreen() {
-  return <div> Welcome to Rich Homes</div>;
+  return (
+    <div className="hero">
+      <div className="hero-text">
+        <h1>
+          <strong>Luxury</strong>
+          <br /> At Its Best
+        </h1>
+        <p>Premium Real Estate At Your Finger Tips</p>
+      </div>
+    </div>
+  );
 }
 
 export default HomeScreen;
