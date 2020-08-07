c39ea42dfb9110e43882b2fa3d2b8e05ab6d3796
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-08-07 18:36:50 +0000

    update

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -3,18 +3,33 @@ import "./HomeScreen.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 
+import Card from "../components/Card";
+
 function HomeScreen() {
   return (
-    <div className="hero">
-      <div className="hero-text">
-        <h1>
-          <span id="c1">Luxury</span>
-          <span id="c2">At Its Best</span>
-        </h1>
-        <p>Premium Real Estate At Your Finger Tips</p>
+    <div>
+      <div className="hero">
+        <div className="hero-text">
+          <h1>
+            <span id="c1">Luxury</span>
+            <span id="c2">At Its Best</span>
+          </h1>
+          <p>Premium Real Estate At Your Finger Tips</p>
+        </div>
+        <div className="hero-icon-container">
+          <FontAwesomeIcon
+            icon={faChevronDown}
+            size="3x"
+            className="hero-icon"
+          />
+        </div>
       </div>
-      <div className="hero-icon-container">
-        <FontAwesomeIcon icon={faChevronDown} size="3x" className="hero-icon" />
+      <div id="featured">
+        <h2 className="featured-title">Featured Properties</h2>
+        <div className="featured-gallery">
+          <Card />
+          <Card />
+        </div>
       </div>
     </div>
   );
