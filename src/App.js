bf9536451f1afd09cb705b1d3c00898cef097a0f
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-08-19 22:05:14 +0000

    footer edit

--- src/App.js
+++ src/App.js
@@ -95,33 +95,35 @@ function App() {
           </div>
         </main>
         <footer className="footer">
-          <div className="footer-top">
-            <h4>RichHomes Properties</h4>
-            <div className="footer-top-item">
-              <p>Services</p>
-              <hr />
-            </div>
-            <div className="footer-top-item">
-              <p>Contact Us</p>
-              <hr />
-              <div className="footer-contact-item">
-                <FontAwesomeIcon icon={faEnvelope} className="footer-icon" />
-                <FontAwesomeIcon icon={faInstagram} className="footer-icon" />
-                <FontAwesomeIcon icon={faFacebook} className="footer-icon" />
+          <div className="footer-container">
+            <div className="footer-top">
+              <h4>RichHomes Properties</h4>
+              <div className="footer-top-item">
+                <p>Services</p>
+                <hr />
+              </div>
+              <div className="footer-top-item">
+                <p>Contact Us</p>
+                <hr />
+                <div className="footer-contact-item">
+                  <FontAwesomeIcon icon={faEnvelope} className="footer-icon" />
+                  <FontAwesomeIcon icon={faInstagram} className="footer-icon" />
+                  <FontAwesomeIcon icon={faFacebook} className="footer-icon" />
+                </div>
               </div>
             </div>
-          </div>
-          <div className="footer-bottom">
-            <p>
-              <span
-                role="img"
-                aria-label="copyright"
-                className="footer-copyrigt"
-              >
-                ©️
-              </span>
-              2020 All Rights Reserved By oltostudios
-            </p>{" "}
+            <div className="footer-bottom">
+              <p>
+                <span
+                  role="img"
+                  aria-label="copyright"
+                  className="footer-copyrigt"
+                >
+                  ©️
+                </span>
+                2020 All Rights Reserved By oltostudios
+              </p>{" "}
+            </div>
           </div>
         </footer>
       </div>
