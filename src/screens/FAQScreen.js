74bf14c3872e0c28d62a623c2feda4fb72ba355a
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-08-25 15:07:30 +0000

    add faq title

--- src/screens/FAQScreen.js
+++ src/screens/FAQScreen.js
@@ -1,6 +1,8 @@
 import React from "react";
+
 import "./FAQScreen.css";
 import Footer from "../components/Footer";
+import FAQ from "../components/FAQ";
 
 function FAQScreen() {
   return (
@@ -8,6 +10,10 @@ function FAQScreen() {
       <div className="faq-hero">
         <h1 className="faq-hero-title">Frequently Asked Questions</h1>
         <button className="faq-button feature-button">Ask a question</button>
+      </div>
+      <div className="faq-section">
+        <h2>FAQ</h2>
+        <FAQ />
       </div>
       <Footer />
     </div>
